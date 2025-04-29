68d9c7d38ff86ca889b3f1f8016d2b3da1315f2d
Mach Ngoc Xuan <machngocxuan2004@gmail.com>
2025-04-29 22:27:34 +0700

    fix bug

--- server/server.js
+++ server/server.js
@@ -13,23 +13,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-// eslint-disable-next-line no-undef
-const PORT = process.env.VITE_PORT || 5000;
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || localhost;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-// Routes
-app.get("/api", (req, res) => {
-  res.json({ message: "Welcome to the API!" });
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
@@ -39,7 +33,6 @@ app.use("/api/carts", cartRoutes);
 app.use("/api/settings", settingRoutes);
 app.use("/api/dashboard", dashboadRoutes);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, host, () => {
+  console.log(`Server is running http://${host}:${port}`);
 });
