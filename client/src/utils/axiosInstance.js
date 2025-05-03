384cba77f82a66c1b21b35481f0f1ca35fb9f0ff
Mach Ngoc Xuan <machngocxuan2004@gmail.com>
2025-05-03 22:56:21 +0700

    fix bug uri backend for client

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.API_URL || `http://localhost:5000`;
+const API_BASE_URL = import.meta.env.API_URL;
+
+console.log(`API_BASE_URL: ${API_BASE_URL}`);
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
